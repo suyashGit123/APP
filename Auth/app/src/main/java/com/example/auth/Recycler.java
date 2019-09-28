b55c7789d2bf85aa99818dda96d95bc8f182303a
suyashGit123 <suyashsardeshpande@gmail.com>
2019-09-28 10:03:20 +0530

    second commit

--- Auth/app/src/main/java/com/example/auth/Recycler.java
+++ Auth/app/src/main/java/com/example/auth/Recycler.java
@@ -1,14 +1,19 @@
 package com.example.auth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import java.util.*;
 
@@ -16,6 +21,7 @@ public class Recycler extends AppCompatActivity {
     DatabaseReference reference;
     ArrayList<Profile> List;
     RecyclerView recyclerView;
+    MyAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +32,25 @@ public class Recycler extends AppCompatActivity {
 
 
         reference = FirebaseDatabase.getInstance().getReference().child("Profiles");
-        refer
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot dataSnapshot1: dataSnapshot.getChildren())
+                {
+                    Profile p = dataSnapshot1.getValue(Profile.class);
+                    List.add(p);
+                }
+                adapter = new MyAdapter(Recycler.this,List);
+                recyclerView.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(Recycler.this,"oops",Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
